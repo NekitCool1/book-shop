c0b7c537f1c06813a96a6041a2c3a365d4095480
NekitCool1 <gnnikita97@gmail.com>
2024-07-11 17:15:37 +0300

    Empty lines were deleted

--- src/main/java/book/shop/dto/BookDto.java
+++ src/main/java/book/shop/dto/BookDto.java
@@ -4,14 +4,9 @@ import java.math.BigDecimal;
 
 public class BookDto {
     private Long id;
-
     private String title;
-
     private String author;
-
     private BigDecimal price;
-
     private String description;
-
     private String coverImage;
 }
