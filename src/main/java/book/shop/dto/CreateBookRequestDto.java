728596a6259e485339c783cdf067f5b73f9dcb92
NekitCool1 <gnnikita97@gmail.com>
2024-07-14 13:07:10 +0300

    Empty lines were deleted #2

--- src/main/java/book/shop/dto/CreateBookRequestDto.java
+++ src/main/java/book/shop/dto/CreateBookRequestDto.java
@@ -3,14 +3,9 @@ package book.shop.dto;
 import java.math.BigDecimal;
 
 public class CreateBookRequestDto {
-
     private String title;
-
     private String author;
-
     private BigDecimal price;
-
     private String description;
-
     private String coverImage;
 }
