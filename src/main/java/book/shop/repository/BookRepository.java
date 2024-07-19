92621e38bd5f679c9abe9fe5cc3686c8f9378fb7
NekitCool1 <gnnikita97@gmail.com>
2024-07-20 00:21:04 +0300

    Merge conflicts were fixed

--- src/main/java/book/shop/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package book.shop.repository;
-
-import book.shop.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
