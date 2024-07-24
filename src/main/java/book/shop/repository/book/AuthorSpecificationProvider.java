48db922f19954254cb769b240d3df9eed5a7df29
NekitCool1 <gnnikita97@gmail.com>
2024-07-24 22:23:25 +0300

    commit

--- src/main/java/book/shop/repository/book/AuthorSpecificationProvider.java
+++ src/main/java/book/shop/repository/book/AuthorSpecificationProvider.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorSpecificationProvider implements SpecificationProvider<Book> {
-    private static final String key = "author";
+    private static final String KEY = "author";
 
     @Override
     public String getKey() {
-        return key;
+        return KEY;
     }
 
     public Specification<Book> getSpecification(String[] params) {
-        return (root, query, criteriaBuilder) -> root.get("author")
+        return (root, query, criteriaBuilder) -> root.get(KEY)
                 .in(Arrays.stream(params)
                         .toArray());
     }
--- src/main/java/book/shop/repository/book/TitleSpecificationProvider.java
+++ src/main/java/book/shop/repository/book/TitleSpecificationProvider.java
@@ -16,7 +16,7 @@ public class TitleSpecificationProvider implements SpecificationProvider<Book> {
     }
 
     public Specification<Book> getSpecification(String[] params) {
-        return (root, query, criteriaBuilder) -> root.get("title")
+        return (root, query, criteriaBuilder) -> root.get(key)
                 .in(Arrays.stream(params)
                         .toArray());
     }
