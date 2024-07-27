4653725758f3172d1af804e7e6e0bacbbbe638e0
NekitCool1 <gnnikita97@gmail.com>
2024-07-27 16:30:17 +0300

    All errors were fixed

--- src/main/java/book/shop/dto/BookDto.java
+++ src/main/java/book/shop/dto/BookDto.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 public record BookDto(Long id,
                       String title,
                       String author,
+                      String isbn,
                       BigDecimal price,
                       String description,
                       String coverImage) {
