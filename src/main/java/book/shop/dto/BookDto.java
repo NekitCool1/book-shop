99d94adc7f1260d021d208c28a724984acae43af
NekitCool1 <103503613+NekitCool1@users.noreply.github.com>
2024-07-27 17:43:18 +0300

    conflicts were fixed (#11)

--- src/main/java/book/shop/dto/BookDto.java
+++ src/main/java/book/shop/dto/BookDto.java
@@ -1,12 +1,15 @@
 package book.shop.dto;
 
 import java.math.BigDecimal;
+import lombok.Data;
 
-public record BookDto(Long id,
-                      String title,
-                      String author,
-                      String isbn,
-                      BigDecimal price,
-                      String description,
-                      String coverImage) {
+@Data
+public class BookDto {
+    private Long id;
+    private String title;
+    private String author;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+    private String isbn;
 }
